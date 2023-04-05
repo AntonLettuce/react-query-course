0edec49632444f13f3b05283356270908de07ffd
AntonLettuce <antonlettuce@gmail.com>
2023-04-05 11:31:16 +0400

    Add labels toggle logic

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,21 @@
-export default function LabelList() {
-  return <h3>Labels</h3>;
+import { useLabelsData } from "../helpers/useLabelsData";
+
+export default function LabelList({ selected, toggle }) {
+  const { data, isLoading } = useLabelsData();
+  return (
+    <>
+      <h3>Labels</h3>
+      <div className="labels">
+        {isLoading ? <p>Loading...</p> : (
+          <ul>
+            {data.map(({ id, color, name }) => (
+              <li key={id}>
+                <button onClick={() => toggle(id)} className={`${selected.includes(id) ? 'selected' : ''} label ${color}`}>{name}</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
 }
