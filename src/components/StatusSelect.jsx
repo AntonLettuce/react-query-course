76bafb3eec1f1cb5b59d36fbfb6dc92675c850de
AntonLettuce <antonlettuce@gmail.com>
2023-04-05 20:59:00 +0400

    Add issue details page

--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,16 +1,10 @@
-const options = [
-  { id: "backlog", label: "Backlog" },
-  { id: "todo", label: "To-do" },
-  { id: "inProgress", label: "In Progress" },
-  { id: "done", label: "Done" },
-  { id: "cancelled", label: "Cancelled" },
-];
+import { possibleStatus } from "../helpers/defaultData"
 
 export const StatusSelect = ({ value, onChange }) => {
   return (
     <select value={value} onChange={onChange} className="status-select">
       <option value="">Select Your Status</option>
-      {options.map(({ id, label }) => <option key={id} value={id}>{label}</option>)}
+      {possibleStatus.map(({ id, label }) => <option key={id} value={id}>{label}</option>)}
     </select>
   )
 }
